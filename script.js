9a5aec16d9621db8ebfc7eb0668c7f5648a831c3
Glitch (hello-webpage) <none>
2020-07-15 19:30:19 +0000

    🚿🐆 Checkpoint ./script.js:18084582/1187

--- script.js
+++ script.js
@@ -4,59 +4,34 @@
  *    noStroke, random, strokeWeight, text, textSize, width
  */
 
-let drop1x, drop1y, drop1d, drop2x, drop2y, drop2d, drop2FallSpeed, drop1FallSpeed;
+
 
 function setup() {
   createCanvas(500, 500);
   colorMode(HSB, 100);
   // Variables for droplet 1
-  drop1x = 200; // or random(width)
-  drop1y = 0; // or random(height)
-  drop1d = 10; // or random(5,15)
-  drop1FallSpeed = 8; // or random(8, 20)
-  
-
-  // Variables for droplet 2
-  drop2x = random(width)
-  drop2y = 0
-  drop2d = 10
-  drop2FallSpeed = 6
+  let drop1 = new RainDrop(20, 8)
+  let drop2 = new RainDrop(15, 6)
+  let drop3 = new RainDrops(8, 5)
 }
 
 function draw() {
   background(0, 0, 95);
   //// Code for droplet 1
   // Move droplet 1
-  drop1y += drop1FallSpeed;
-  drop2y += drop2FallSpeed
-  // If it goes off the screen...
-  if (drop1y > height) {
-    // ...reset it...
-    drop1y = 0;
-    // ...and move it somewhere random.
-    drop1x = random(width);
+  drop1.y += drop1.fallSpeed
+  if (drop1.y > height) {
+    drop1.y = 0
   }
-   if (drop2y > height) {
-    // ...reset it...
-    drop2y = 0;
-    // ...and move it somewhere random.
-    drop2x = random(width);
-  }
-  // Display droplet 1
-  noStroke();
-  fill(60, 80, 80);
-  ellipse(drop1x, drop1y, drop1d);
 
-  //// Code for droplet 2
-  // Code your next droplet here
-  ellipse(drop2x, drop2y, drop2d)
-}
+// define Raindrop class 
 
-class rainDrop {
-  constructor(d) {
+class RainDrop {
+  constructor(d, fallSpeed) {
     this.x = random(width)
-    this.y = random(height)
+    this.y = 0
     this.d = d
+    this.fallSpeed = fallSpeed
   }
   
   
