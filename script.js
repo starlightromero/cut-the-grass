e7aed8bbcd98e39bc4e9bda2fbbcda13d8aed497
Glitch (hello-webpage) <none>
2020-07-15 19:52:23 +0000

    🏐👖 Checkpoint ./script.js:18084582/1831

--- script.js
+++ script.js
@@ -4,35 +4,68 @@
  *    noStroke, random, strokeWeight, text, textSize, width
  */
 
-
+let drop1, drop2, drop3, drop4, drop5, drop6, drop7
 
 function setup() {
   createCanvas(500, 500);
   colorMode(HSB, 100);
   // Variables for droplet 1
-  let drop1 = new RainDrop(20, 8)
-  let drop2 = new RainDrop(15, 6)
-  let drop3 = new RainDrops(8, 5)
+  drop1 = new RainDrop(12)
+  drop2 = new RainDrop(15)
+  drop3 = new RainDrop(8)
+  drop4 = new RainDrop (4)
+  drop5 = new RainDrop (10)
+  drop6 = new RainDrop (3)
+  drop7 = new RainDrop (6)
 }
 
 function draw() {
   background(0, 0, 95);
-  //// Code for droplet 1
-  // Move droplet 1
-  drop1.y += drop1.fallSpeed
-  if (drop1.y > height) {
-    drop1.y = 0
-  }
+
+  // Move droplets
+
+  drop1.drip()
+  drop2.drip()
+  drop3.drip()
+  drop4.drip()
+  drop5.drip()
+  drop6.drip()
+  drop7.drip()
+ 
+//   display droplets
+  drop1.show()
+  drop2.show()
+  drop3.show()
+  drop4.show()
+  drop5.show()
+  drop6.show()
+  drop7.show()
+
+}
 
 // define Raindrop class 
 
 class RainDrop {
-  constructor(d, fallSpeed) {
+  constructor(d) {
     this.x = random(width)
     this.y = 0
     this.d = d
-    this.fallSpeed = fallSpeed
+    this.fallSpeed = 0.8 * d
   }
   
+  show() {
+    //   Display droplets 
+  noStroke()
+  fill(60, 80, 80)
+  ellipse(this.x, this.y, this.d)
+  }
   
+  drip() {
+    this.y += this.fallSpeed
+    
+    if (this.y > height) {
+      this.y = 0
+      this.x = random(width)
+    }
+  } 
 }
