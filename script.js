a8ad3920d756836d8b423b8a28a2716767ae8655
Glitch (hello-webpage) <none>
2020-07-15 19:18:29 +0000

    📍📷 Checkpoint ./script.js:18084582/498

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
  *    noStroke, random, strokeWeight, text, textSize, width
  */
 
-let drop1x, drop1y, drop1d, drop1FallSpeed;
+let drop1x, drop1y, drop1d, drop2x, drop2y, drop2d, drop2FallSpeed, drop1FallSpeed;
 
 function setup() {
   createCanvas(500, 500);
@@ -14,8 +14,13 @@ function setup() {
   drop1y = 0; // or random(height)
   drop1d = 10; // or random(5,15)
   drop1FallSpeed = 8; // or random(8, 20)
+  
 
   // Variables for droplet 2
+  drop2x = random(width)
+  drop2y = 0
+  drop2d = 10
+  drop2FallSpeed = 6
 }
 
 function draw() {
@@ -23,12 +28,19 @@ function draw() {
   //// Code for droplet 1
   // Move droplet 1
   drop1y += drop1FallSpeed;
+  drop2y += drop2FallSpeed
   // If it goes off the screen...
   if (drop1y > height) {
     // ...reset it...
     drop1y = 0;
     // ...and move it somewhere random.
     drop1x = random(width);
+  }
+   if (drop2y > height) {
+    // ...reset it...
+    drop2y = 0;
+    // ...and move it somewhere random.
+    drop2x = random(width);
   }
   // Display droplet 1
   noStroke();
@@ -37,4 +49,15 @@ function draw() {
 
   //// Code for droplet 2
   // Code your next droplet here
+  ellipse(drop2x, drop2y, drop2d)
+}
+
+class rainDrop {
+  constructor(d) {
+    this.x = random(width)
+    this.y = random(height)
+    this.d = d
+  }
+  
+  
 }
